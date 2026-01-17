d38d51e1103f8ebcc61fa191cccc193ab6c0bfab
VyMongDu <92016512+anego0201@users.noreply.github.com>
2026-01-17 21:34:59 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		System.out.println("Where have you been");
 		System.out.println("this is for testing commit and push");
 		System.out.println("Đây là code test ở nhà để test commit máy ở nhà");
+		sysout.out.println("Còn đây là để test merge từ github về máy");
 	}
 }
