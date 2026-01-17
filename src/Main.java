b863cb66607135a4b835ebd485900bd3d02f9555
anego0201 <lyhoanganhvy4392@gmail.com>
2026-01-17 21:39:27 +0900

    test merge chưa hiểu

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("this is for testing commit and push");
 		System.out.println("Đây là code test ở nhà để test commit máy ở nhà");
 		System.out.println("này là để xem là nếu chưa merge file thì commit sẽ bị như thế nào!");
+		System.out.println("là sao chưa hiểu ta");
 	}
 }
