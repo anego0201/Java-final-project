6cc4397c368b888503da555ebd0f3440b8585d6b
anego0201 <lyhoanganhvy4392@gmail.com>
2026-01-17 21:35:56 +0900

    commit test khi chưa merge mà sửa

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		System.out.println("Where have you been");
 		System.out.println("this is for testing commit and push");
 		System.out.println("Đây là code test ở nhà để test commit máy ở nhà");
+		System.out.println("này là để xem là nếu chưa merge file thì commit sẽ bị như thế nào!");
 	}
 }
