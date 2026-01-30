65d34324d927103a3d7791f82ed8f41a9aa58f99
VyMongDu <lyhoanganhvy4392@gmail.com>
2026-01-30 10:20:50 +0900

    làm lại sạch code, viết outline về 2 vấn đề cần xử lý và hướng đi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-	
+//	とりあえず、どんな問題を解決するかきめよう！
+
+//-------------------------------------------------------
+//	1. FIRST PROBLEM
+//	Solve "not enough change" problem, with the downward count math
+//	# use the bigest bill can hand back to customer first than the small bills will come after
+//	# With this way, small bill will me minimun use
+//	# To be more advance
+//		-> put in a notification whenever the bill left is under a certain amount
+//		-> notification if the change is not enough to hand back to customer
+
+
+//-------------------------------------------------------
+//	2. SECOND PROBLEM
+//	Canceling come when the rush hour arrive!
+
+//	#Queue list will need to be implement for customer oder
+//	-> Customer class
+//		- Bill number
+//		- Food list
+//		- Time order (To calculate the time and display on the recipt for customer to know how long will it take)
+
+
+// 	#To solve and minimize returning, Put in queue and display it on the screen
+//	so customer can know exact how long they will have to wait
+
+//	# Numer Queue	(is this needed?)
+//	-> display number queue how many order left!
+//	-> currently which order is called?
+
+
+//	# Time Queue
+//	->	how much compacity the steamer can hold
+//	-> 	is there is available room for the current customer order?	
+//	-> after all the observation, time queue will be shown in font of the cashier so the customer can decide
+//	whether buy or not!
+
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Where have you been");
-		System.out.println("this is for testing commit and push");
-		System.out.println("Đây là code test ở nhà để test commit máy ở nhà");
-		System.out.println("này là để xem là nếu chưa merge file thì commit sẽ bị như thế nào!");
-		System.out.println("là sao chưa hiểu ta");
-		System.out.println("Còn đây là để test merge từ github về máy");
-		System.out.println("im here to see if the pushing work");
-		System.out.println("Test ngay 30/1");
 	
 	}
 }
